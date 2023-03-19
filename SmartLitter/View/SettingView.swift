4cde643e6cc074bd56ba10c71bf5f3f43ac27095
Forrest Zhang <yunyuzhang11@gmail.com>
2023-03-19 19:20:17 -0400

    Added Setting view

--- SmartLitter/View/SettingView.swift
+++ SmartLitter/View/SettingView.swift
@@ -8,8 +8,109 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State private var toggleOn = true
+    
+    @State private var numberOfTime: String = ""
+    @State private var hour: String = ""
+    
+
     var body: some View {
-        Text("Setting")
+        NavigationView {
+            ScrollView {
+                ZStack {
+                    VStack(alignment: .leading) {
+                        HStack() {
+                            Image(systemName: "person")
+                            Text("Forrest Zhang")
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
+                        }
+                        .frame(width: 150, height: 70)
+                        Divider()
+                        
+                        VStack(spacing:20) {
+                            
+                            VStack {
+                                Text("Account Settings")
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .frame(maxWidth: .infinity,
+                                           maxHeight: .infinity,
+                                           alignment: .leading)
+                                
+                                HStack {
+                                    Text("Edit User Profile")
+                                    Spacer()
+                                    Image(systemName: "chevron.forward")
+                                }
+                                .frame(maxWidth: .infinity,
+                                       maxHeight: .infinity,
+                                       alignment: .leading)
+                            }
+                            Divider()
+                            
+                            VStack() {
+                                Text("Notification Settings")
+                                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                                    .frame(maxWidth: .infinity,
+                                           maxHeight: .infinity,
+                                           alignment: .leading)
+                                Toggle("All Notifications", isOn: $toggleOn)
+                                Toggle("Notify when there is a usage", isOn: $toggleOn)
+                                Group {
+                                    Text("Notify me when:")
+                                        .frame(maxWidth: .infinity,
+                                               maxHeight: .infinity,
+                                               alignment: .leading)
+                                    SegmentedControlView(options: ["cat1", "cat2"], selected: "cat1")
+                                    SegmentedControlView(options: ["Poop", "Pee"], selected: "Poop")
+                                    Text("For")
+                                    SegmentedControlView(options: ["More than", "Less or equals to"], selected: "More than")
+                                }
+                                
+                                HStack {
+                                    TextField(
+                                            "Number of times",
+                                            text: $numberOfTime
+                                        )
+                                    .frame(width: 200, height: 50)
+                                    Text("times")
+                                }
+                                Text("Within")
+                                
+                                HStack {
+                                    TextField(
+                                            "Hours",
+                                            text: $hour
+                                        )
+                                    .frame(width: 200, height: 50)
+                                    Text("Hours")
+                                }
+                                
+                                Text("Save Setting")
+                                    .frame(width: 150, height: 30)
+                                    .font(.system(size: 15, weight: .light, design: .rounded))
+                                    .padding(10)
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(20)
+                            }
+                
+                        }
+                        
+                
+        
+    
+                    }
+                    .font(.system(size: 20, weight: .light, design: .rounded))
+                    .frame(maxWidth: .infinity,
+                           maxHeight: .infinity,
+                           alignment: .leading)
+                    .padding()
+                   
+                }
+                
+            }
+            
+        }
     }
 }
 
