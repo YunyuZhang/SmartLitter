8de9162598977ece873f1eb53770e2bdda0794ed
Forrest Zhang <yunyuzhang11@gmail.com>
2023-03-19 16:39:39 -0400

    Project init. Added UI for Cat Profile and Edit Cat Profile views

--- SmartLitter/View/CatProfileView.swift
+++ SmartLitter/View/CatProfileView.swift
@@ -7,11 +7,81 @@
 
 import SwiftUI
 
+
 struct CatProfileView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Text("Cat Profile")
+                        .padding(30)
+                    Image("haybe_profile")
+                        .resizable()
+                        .clipShape(Circle())
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height:200)
+                    Text("Haybe")
+                        .frame(width: 100, height: 40)
+                        .offset(x:0, y: -10)
+                    NavigationLink(destination: EditProfileView()) {
+                                       Text("Edit Profile")
+                                           .frame(width: 150, height: 30)
+                                           .font(.system(size: 20, weight: .light, design: .rounded))
+                                           .padding(10)
+                                           .background(Color.blue)
+                                           .foregroundColor(.white)
+                                           .cornerRadius(20)
+                                   }
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("Age: 2 years old")
+                        Text("Breed: Brisith Short Hair")
+                        Text("Weight: 14 Lbs")
+                        Text("Last Updated: 3/19/2022")
+                    }
+                    .padding(.top, 10)
+                    
+                    Text("See Weight Data")
+                        .frame(width: 150, height: 30)
+                        .font(.system(size: 20, weight: .light, design: .rounded))
+                        .padding(15)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .padding(.top, 30)
+                }
+            }
+            .font(.system(size: 20, weight: .light, design: .rounded))
+        }
+        .navigationTitle("Cat Profile")
+        .accentColor(.blue)
     }
 }
+
+struct ProfileText: View {
+//    @AppStorage("name") var name = DefaultSettings.name
+//    @AppStorage("subtitle") var subtitle = DefaultSettings.subtitle
+//    @AppStorage("description") var description = DefaultSettings.description
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            VStack(spacing: 5) {
+                Text("name")
+                    .bold()
+                    .font(.title)
+                Text("subtitle")
+                    .font(.body)
+                    .foregroundColor(.secondary)
+            }.padding()
+            Text("description")
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
+        }
+    }
+}
+
 
 struct CatProfileView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SmartLitter/View/EditProfileView.swift
@@ -0,0 +1,84 @@
+//
+//  EditProfileView.swift
+//  SmartLitter
+//
+//  Created by Forrest on 3/19/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    
+    @State private var catName: String = ""
+    @State private var age: String = ""
+    @State private var breed: String = ""
+    @State private var weight: String = ""
+    
+    var body: some View {
+        NavigationView() {
+            ScrollView {
+                VStack {
+                    Text("Edit Cat Profile")
+                        .padding(30)
+                    Image("haybe_profile")
+                        .resizable()
+                        .clipShape(Circle())
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height:200)
+                    Text("Edit profile picture")
+                        .frame(width: 300, height: 40)
+                        .offset(x:0, y:-20)
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Pet's name")
+                            TextField(
+                                    "Name",
+                                    text: $catName
+                                )
+                            .frame(width: 200, height: 50)
+                        }
+                        HStack {
+                            Text("Age")
+                            TextField(
+                                    "Age",
+                                    text: $catName
+                                )
+                            .frame(width: 200, height: 50)
+                        }
+                        HStack {
+                            Text("Breed")
+                            TextField(
+                                    "Breed",
+                                    text: $breed
+                                )
+                            .frame(width: 200, height: 50)
+                        }
+                        HStack {
+                            Text("Weight")
+                            TextField(
+                                    "Weight(lbs)",
+                                    text: $weight
+                                )
+                            .frame(width: 200, height: 50)
+                        }
+                    }
+                    Text("Update Profile")
+                        .frame(width: 150, height: 30)
+                        .padding(15)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                        .padding(.top, 30)
+                }
+                .font(.system(size: 20, weight: .light, design: .rounded))
+            }
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
