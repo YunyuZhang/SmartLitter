ec89bb2afd87723942162b917eb8958f129d5f60
Forrest Zhang <yunyuzhang11@gmail.com>
2023-03-18 21:11:50 -0400

    Initial Commit

---
+++ SmartLitter/SmartLitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartLitterApp.swift
+//  SmartLitter
+//
+//  Created by Forrest on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartLitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
