f7d48ede33e402b0cd71856c351cc26bbb07a44f
Forrest Zhang <yunyuzhang11@gmail.com>
2023-04-23 23:01:05 -0400

    Tried to make chart interactive with filter. Not successful
    But the filterList is not updated when the picker is changed

--- SmartLitter/View/CatProfile/WeightInfoView.swift
+++ SmartLitter/View/CatProfile/WeightInfoView.swift
@@ -1,34 +1,40 @@
-//
-//  WeightInfoView.swift
-//  SmartLitter
-//
-//  Created by Forrest on 3/19/23.
-//
 
 import SwiftUI
 import Charts
 
 struct WeightInfoView: View {
-    struct weight: Identifiable {
-            let id = UUID()
-            let weekday: Date
-            let weight: Int
-            
-            init(day: String, weight: Int) {
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyyMMdd"
-                self.weekday = formatter.date(from: day) ?? Date.distantPast
-                self.weight = weight
-            }
-        }
-    
-    let weightHistory: [weight] = [
-        weight(day: "20211001", weight: 6),
-        weight(day: "20211101", weight: 12),
-        weight(day: "20211201", weight: 13),
-        weight(day: "20220201", weight: 13),
-        weight(day: "20220301", weight: 18)
+
+    let allWeightHistory: [Weight] = [
+        Weight(day: "20210324", weight: 3),
+        Weight(day: "20211001", weight: 6),
+        Weight(day: "20211101", weight: 12),
+        Weight(day: "20211201", weight: 13),
+        Weight(day: "20220201", weight: 13),
+        Weight(day: "20220301", weight: 18),
+        Weight(day: "20220401", weight: 19),
+        Weight(day: "20220501", weight: 20),
+        Weight(day: "20220601", weight: 22),
+        Weight(day: "20220701", weight: 23),
+        Weight(day: "20220801", weight: 24),
+        Weight(day: "20220901", weight: 23),
+        Weight(day: "20221001", weight: 22),
+        Weight(day: "20221101", weight: 20),
+        Weight(day: "20221201", weight: 19),
+        Weight(day: "20230101", weight: 18),
+        Weight(day: "20230201", weight: 17),
+        Weight(day: "20230301", weight: 16),
+        Weight(day: "20230401", weight: 17),
+        Weight(day: "20230410", weight: 16.5),
+        Weight(day: "20230415", weight: 16),
+        Weight(day: "20230420", weight: 16.2),
+        Weight(day: "20230425", weight: 19)
     ]
+
+    
+    @State private var selectedOption = "1m"
+    let options = ["1w", "1m", "6m", "1ydada"]
+    
+    @State var filteredWeightHistory: [Weight] = []
         
     var body: some View {
         NavigationView {
@@ -42,7 +48,18 @@ struct WeightInfoView: View {
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 200, height:200)
                     
-                    SegmentedControlView(options: ["1w", "1m", "6m", "1y"], selected: "1m")
+                    
+                    Picker("Options", selection: $selectedOption) {
+                                    ForEach(0..<options.count) { index in
+                                        Text(options[index])
+                                    }
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                               
+                    
+//                    SegmentedControlView(options: ["1w", "1m", "6m", "1y"], selected: selectedOption)
+                
+                    
                     
                     Chart {
                         RectangleMark(
@@ -54,7 +71,7 @@ struct WeightInfoView: View {
                         .foregroundStyle(.green)
                         .opacity(0.5)
                         
-                        ForEach(weightHistory) {
+                        ForEach(filteredWeightHistory, id: \.id) {
                             LineMark(
                                 x: .value("Week Day", $0.weekday, unit: .day),
                                 y: .value("weight", $0.weight)
@@ -77,12 +94,39 @@ struct WeightInfoView: View {
                 }
             }
         }
+        .onAppear {
+            filteredWeightHistory = getFilteredWeightHistory(info:"onAppear")
+        }
+    }
+    
+    private func getFilteredWeightHistory(info: String) -> [Weight] {
         
+        print("++++ called from: \(info)")
+        switch selectedOption {
+        case "1w":
+            return allWeightHistory.filter { $0.weekday > Date().addingTimeInterval(-7*24*60*60) }
+        case "1m":
+            return allWeightHistory.filter { $0.weekday > Date().addingTimeInterval(-30*24*60*60) }
+        case "6m":
+            return allWeightHistory.filter { $0.weekday > Date().addingTimeInterval(-180*24*60*60) }
+        case "1y":
+            return allWeightHistory.filter { $0.weekday > Date().addingTimeInterval(-365*24*60*60) }
+        default:
+            return allWeightHistory
+        }
     }
 }
 
-struct WeightInfoView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeightInfoView()
+struct Weight: Identifiable {
+    let id = UUID()
+    let weekday: Date
+    let weight: Double
+    
+    init(day: String, weight: Double) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        self.weekday = formatter.date(from: day) ?? Date.distantPast
+        self.weight = weight
     }
 }
+
